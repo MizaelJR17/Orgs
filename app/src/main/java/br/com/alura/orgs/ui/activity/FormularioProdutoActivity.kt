949a07634aa9f2e2c45c992abc2ed8f71f0d6ba0
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2022-09-22 17:04:46 -0300

    Crindo Tela de Lists de Produtos

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,10 @@
+package br.com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+
+}
